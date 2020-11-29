6913a3a74cd030ddaccb9652eb69e2f97b118f69
Ilya Maximov <mail@overlisted.net>
2020-11-29 17:43:16 +0100

    add some logic to the example

--- example/example.c
+++ example/example.c
@@ -1,9 +1,12 @@
 #define CNFGOGL
 
+#include <stdio.h>
+
 #define ONLYINPUT_IMPLEMENTATION
 #define CNFG_IMPLEMENTATION
 #include <../onlyinput.h>
 #include <rawdraw/CNFG.h>
+#include <rawdraw/os_generic.h>
 
 void HandleKey(int keycode, int bDown) {
   OIMaintain(keycode, bDown);
@@ -22,5 +25,16 @@ void HandleDestroy() {
 }
 
 int main() {
-  return 0;
+  CNFGSetup("Example", 800, 600);
+
+  while(1) {
+    CNFGClearFrame();
+    CNFGHandleInput();
+
+    printf("Pressed letter: %c\n", OIReadAscii());
+
+    CNFGSwapBuffers();
+
+    OGSleep(1);
+  }
 }
